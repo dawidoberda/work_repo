ab412b488d0d44cde400055dd5b110b8c9cf62b6
Dawid Oberda <dawidoberda@gmail.com>
2019-07-12 21:26:49 +0200

    aktualizacja mpn organizera

--- MPN&HS&ECN_organizer/Parser_csv.py
+++ MPN&HS&ECN_organizer/Parser_csv.py
@@ -1,10 +1,16 @@
 import csv
 import os
 
+
 class ParserCsv:
-    def __init__(self, first_file_name, second_file_name):
+
+    actual_line_qty_first = 0
+    actual_line_qty_second = 0
+
+    def __init__(self, first_file_name, second_file_name, compare_file_name):
         self.first_file_name = first_file_name
         self.second_file_name = second_file_name
+        self.compare_file_name = compare_file_name
 
     def check_if_exist(self):
         file_exist_indicator = [False, False]
@@ -26,6 +32,35 @@ class ParserCsv:
         return file_exist_indicator
 
     def create_first_temp(self):
-        with open(self.first_file_namem, 'r') as f:
+        with open(self.first_file_name, 'r') as f:
             csv_one = csv.reader(f, delimiter=',')
-            #TODO: sprobowac teraz otworzyc obiekt (kolejny csv) do zapisu w ktorym do nowego pliku zapiszemy tylko te z 0 kolumna = 'ENG:ESD data'
+            first_file_name_part = self.first_file_name.split(".")
+            first_name = first_file_name_part[0] + '_temp.csv'
+
+            with open(first_name, 'w') as w:
+                csv_write_one = csv.writer(w, delimiter=',')
+
+                for line in csv_one:
+                    actual_line_qty_first = actual_line_qty_first+1
+                    if (line[0] == 'ENG:ESD data' and line[10] == 'Axis'):
+                        csv_write_one.writerow(line)
+
+    def create_second_temp(self):
+        with open(self.second_file_name, 'r') as f2:
+            csv_two = csv.reader(f2, delimiter=',')
+            second_file_name_part = self.second_file_name.split(".")
+            second_name = second_file_name_part[0] + '_temp.csv'
+
+            with open(second_name, 'w') as w2:
+                csv_write_two = csv.writer(w2, delimiter=',')
+
+                for line in csv_two:
+                    if (line[0] == 'ENG:ESD data' and line[10] == 'Axis'):
+                        csv_write_two.writerow(line)
+
+    def compare_csv(self):
+        pass
+        #print(actual_line_qty_first)
+            #TODO: sprawdzic dlaeczego nie dziala zmienna klasowa.
+        # Nastepnie sprawdzic ile lini ma plik 1 i 2 pamietac ze potrzebny jest jakis csv ktory bedzie zbieral dane z
+        # kolejnych dni i je zapamietywal (sztucznie go stworzyc) i porowniaine 2 sasiadujacych dni ma byc zapisywane do tego pliku
--- MPN&HS&ECN_organizer/main.py
+++ MPN&HS&ECN_organizer/main.py
@@ -4,11 +4,18 @@ from Parser_csv import ParserCsv
 def main():
     first_file = 'MPN_previous (copy).csv'
     second_file = 'MPN_today (copy).csv'
-    csv_parser = ParserCsv(first_file, second_file)
+    mpn_compare_file = 'MPN_compare.csv'
+    csv_parser_mpn = ParserCsv(first_file, second_file, mpn_compare_file)
 
     indicators = []
-    indicators = csv_parser.check_if_exist()
+    indicators = csv_parser_mpn.check_if_exist()
     print(indicators)
+
+    if indicators[0] == True:
+        csv_parser_mpn.create_first_temp()
+
+    if indicators[1] == True:
+        csv_parser_mpn.create_second_temp()
 
 
 if __name__=='__main__':
