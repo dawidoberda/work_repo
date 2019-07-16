af6e518f2c99af0907376bf80ed8442fb1c3a6a0
Dawid Oberda <dawidoberda@gmail.com>
2019-07-16 20:59:54 +0200

    aktualizacja klas do obslugi bazy danych

--- MPN&HS&ECN_organizer/datebase_manager.py
+++ MPN&HS&ECN_organizer/datebase_manager.py
@@ -19,6 +19,14 @@ except sqlite3.OperationalError:
 # c.execute("INSERT INTO mpn VALUES ('2019-06-26', 124, 0, 0)") #dodaje do tabeli rekord
 # conn.commit() #dobrze jest to wywolac po kazdym query
 
+#przyklad funkcji
+def insert_mpn(date,qty,new_qty,done_qty):
+    with conn: #mozna uzyc contex menagera i nie trzeba robic commit
+        c.execute("INSERT INTO mpn VALUES (:current_date, :total_qty, :new, :done)",
+                  {'current_date':date, 'total_qty':qty, 'new':new_qty, 'done':done_qty})
+
+# insert_mpn("2019-07-16",114,2,15)
+
 #aby dodac do tabeli rekordy korzystajac ze zmiennych:
 # data1 = '2019-06-27'
 # total1 = 127
@@ -32,6 +40,9 @@ except sqlite3.OperationalError:
 c.execute("SELECT * FROM mpn") #query do bazy danych
 # c.execute("SELECT * FROM mpn WHERE current_date='2019-06-26'") #query do bazy danych
 # c.execute("SELECT * FROM mpn WHERE new=0") #query do bazy danych
+
+# searched_done = 3
+# c.execute("SELECT * FROM mpn WHERE done=?", (searched_done,)) #mozna tez szukac po zmiennej (jezeli jest jedna zmienna to i tak musi byc na koncu "," bo to ma byc tupple
 
 
 # print(c.fetchone()) #zwraca jeden rekord na raz (mozna iterowac)
